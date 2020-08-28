109a9d4e390a6514055f4d33a438d7dabceb9841
estefanimoran4590 <68919382+estefanimoran4590@users.noreply.github.com>
2020-08-28 13:49:58 -0300

    "first commit again"

---
+++ js/login.js
@@ -0,0 +1,18 @@
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+
+
+//document.addEventListener("DOMContentLoaded", function(e){
+
+//});
+
+
+function guardarlogin (user, pass){
+    if(user.trim() === "" || pass.trim() === "") {
+        alert("Rellena los espacios con tus datos.");
+
+    } else { 
+        location.href = "cover.html";
+    }
+}
